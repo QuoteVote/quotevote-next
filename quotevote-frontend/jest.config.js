171ba44458a1728da55af26bcd16bb2831dff834
gunjanghate <ghategunjan@gmail.com>
2025-12-03 18:38:19 +0530

    chore: removed extra files

--- quotevote-frontend/jest.config.js
+++ quotevote-frontend/jest.config.js
@@ -2,27 +2,26 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'node',
+
+  // No Testing Library setup anymore
+  setupFilesAfterEnv: [],
+
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you based on your tsconfig.json paths)
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+
   testMatch: [
     '**/__tests__/**/*.test.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
+
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
@@ -30,26 +29,25 @@ const customJestConfig = {
     '!src/**/__tests__/**',
     '!src/**/__mocks__/**',
   ],
+
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  // Transform files
+
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
       },
-    }],
+    ],
   },
-  // Module file extensions
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  // Ignore patterns
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  // Clear mocks between tests
+
   clearMocks: true,
-  // Restore mocks after each test
   restoreMocks: true,
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
-
